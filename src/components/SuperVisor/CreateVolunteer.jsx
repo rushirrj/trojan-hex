a6cc033fa5d3a592a4e1f0c84758f34a5b7eeba0
ajit <ajitdakre05@gmail.com>
2022-03-28 02:45:12 +0530

    Updated Volunteer

--- src/components/SuperVisor/CreateVolunteer.jsx
+++ src/components/SuperVisor/CreateVolunteer.jsx
@@ -1,85 +1,219 @@
 import React from "react";
 import { useState } from "react";
+import abiArray from "../../utils/abiArray.json";
 import { getAccountID, intializeContract } from "../../utils/connectWallet";
+import { Link } from "react-router-dom";
 const CreateVolunteer = () => {
   const [inputs, setinputs] = useState({
-    name: "", 
-    address: ""
+    name: "",
+    address: "",
   });
-  const contractAddress = '0x21cb42De23aFac678CeD8482E04D4B3699288767';
+  const contractAddress = "0x21cb42De23aFac678CeD8482E04D4B3699288767";
   const contract = intializeContract(abiArray, contractAddress);
   const adminAddress = getAccountID();
 
-  const onChange = (e) =>{
-    setinputs({...inputs, [e.target.name]: e.target.value})
-  }
+  const onChange = (e) => {
+    setinputs({ ...inputs, [e.target.name]: e.target.value });
+  };
 
   //form create volunteer
   const createVolunteer = async (address, name) => {
-    const createNewVolunteer = await contract.methods.appointVolunteers(address, name).send({from : adminAddress});
+    const createNewVolunteer = await contract.methods
+      .appointVolunteers(address, name)
+      .send({ from: adminAddress });
     console.log(createNewVolunteer);
-  }
+  };
 
   //button to recieve
   const receiveSuppliesFromNgo = async (indexOfSupply) => {
-    const getSupplies = await contract.methods.receiveSupples(indexOfSupply).send({from: adminAddress});
+    const getSupplies = await contract.methods
+      .receiveSupples(indexOfSupply)
+      .send({ from: adminAddress });
     console.log(getSupplies);
-  }
+  };
 
   //
-  const getAllVolunteers = async (Supervisor_address) =>{
-    const getSize = await contract.methods.getSizeOffetchVolunteers(address).call();
-    for(let i = 0; i < getSize; i++){
-      let volunteer = await contract.methods.fetchVolunteers(Supervisor_address, i).call();
-      let volunteer_name = await contract.methods.getVolunteers(volunteer).call();
+  const getAllVolunteers = async (Supervisor_address) => {
+    const getSize = await contract.methods
+      .getSizeOffetchVolunteers(address)
+      .call();
+    for (let i = 0; i < getSize; i++) {
+      let volunteer = await contract.methods
+        .fetchVolunteers(Supervisor_address, i)
+        .call();
+      let volunteer_name = await contract.methods
+        .getVolunteers(volunteer)
+        .call();
       console.log(volunteer, volunteer_name);
     }
-  } 
-
-
+  };
 
   return (
-    <section className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-col text-center w-full mb-12">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-            Add Volunteer to Group/NGO.
-          </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-            gentrify, subway tile poke farm-to-table. Franzen you probably
-            haven't heard of them man bun deep.
-          </p>
-        </div>
-        <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
-          <div className="relative flex-grow w-full">
-            <label for="full-name" className="leading-7 text-sm text-gray-600">
-              Address
-            </label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
+    <>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+              Add Volunteer to Group/NGO.
+            </h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              haven't heard of them man bun deep.
+            </p>
           </div>
-          <div className="relative flex-grow w-full">
-            <label for="email" className="leading-7 text-sm text-gray-600">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
+          <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
+            <div className="relative flex-grow w-full">
+              <label
+                for="full-name"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative flex-grow w-full">
+              <label for="email" className="leading-7 text-sm text-gray-600">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Submit
+            </button>
           </div>
-          <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Submit
-          </button>
         </div>
+      </section>
+      <div className="md:mx-auto md:10/12 lg:w-8/12 mb-10">
+        <div className="flex justify-between mb-3">
+          <Link
+            class="mt-3 text-indigo-500 inline-flex items-center"
+            to="/supervisor"
+          >
+            Manage Volunteer
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </Link>
+          <Link
+            class="mt-3 text-indigo-500 inline-flex items-center"
+            to="/supervisor/manage"
+          >
+            Manage Supplies
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </Link>
+        </div>
+        <table className="table-auto w-full text-left whitespace-no-wrap">
+          <thead>
+            <tr>
+              <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                Volunteer Name
+              </th>
+              <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                Address
+              </th>
+              <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                Rewards
+              </th>
+              <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                Community Hours
+              </th>
+              <th className="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="px-4 py-3">Ajit NGO</td>
+              <td className="px-4 py-3">
+                0xe3B3f5ace203d5659eEb0133dec972921ca9bB21
+              </td>
+              <td className="px-4 py-3 text-lg text-gray-900">Free</td>
+              <td className="px-4 py-3 text-lg text-gray-900">6hrs</td>
+              <td className="w-10 text-center">
+                <input name="plan" type="radio" />
+              </td>
+            </tr>
+            <tr>
+              <td className="border-t-2 border-gray-200 px-4 py-3">RRJ NGO</td>
+              <td className="border-t-2 border-gray-200 px-4 py-3">
+                0xe3B3f5ace203d5659eEb0133dec972921ca9bB21
+              </td>
+              <td className="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                $24
+              </td>
+              <td className="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                10hrs
+              </td>
+              <td className="border-t-2 border-gray-200 w-10 text-center">
+                <input name="plan" type="radio" />
+              </td>
+            </tr>
+            <tr>
+              <td className="border-t-2 border-gray-200 px-4 py-3">
+                Tejas PIRO Coder
+              </td>
+              <td className="border-t-2 border-gray-200 px-4 py-3">
+                0xe3B3f5ace203d5659eEb0133dec972921ca9bB21
+              </td>
+              <td className="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                $50
+              </td>
+              <td className="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                5hrs
+              </td>
+              <td className="border-t-2 border-gray-200 w-10 text-center">
+                <input name="plan" type="radio" />
+              </td>
+            </tr>
+            <tr>
+              <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">
+                Ashutosh
+              </td>
+              <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3">
+                0xe3B3f5ace203d5659eEb0133dec972921ca9bB21
+              </td>
+              <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                $72
+              </td>
+              <td className="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                7hrs
+              </td>
+              <td className="border-t-2 border-b-2 border-gray-200 w-10 text-center">
+                <input name="plan" type="radio" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </section>
+    </>
   );
 };
 
--- src/pages/ManageVolunteer.jsx
+++ src/pages/ManageVolunteer.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import CreateVolunteer from "../components/SuperVisor/CreateVolunteer";
-import VTable from "../components/SuperVisor/VTable";
 
 const ManageSupervisor = () => {
   return (
     <div>
       <CreateVolunteer />
-      <div className="md:mx-auto md:10/12 lg:w-8/12 mb-10">
-        <VTable />
-      </div>
     </div>
   );
 };
