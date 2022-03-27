b59050b9e3313dce8046d555702ed3a1a2d87bc6
ajit <ajitdakre05@gmail.com>
2022-03-27 17:45:43 +0530

    Added Routes

--- src/Routes.js
+++ src/Routes.js
@@ -3,17 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NGO from "./pages/NGO";
 import Admin from "./pages/Admin";
-import ManageSupervisor from "./pages/ManageSupervisor";
+import ManageVolunteer from "./pages/ManageVolunteer";
 import ManageSupplySupervisor from "./pages/ManageSupplySupervisor";
 import { Outlet } from "react-router-dom";
 const RoutesM = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/ngo" element={<NGO />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path="/ngo" element={<Outlet />}>
+        <Route path="" element={<NGO />} />
+      </Route>
       <Route path="/supervisor" element={<Outlet />}>
-        <Route path="" element={<ManageSupervisor />} />
+        <Route path="" element={<ManageVolunteer />} />
         <Route path="manage" element={<ManageSupplySupervisor />} />
       </Route>
     </Routes>
